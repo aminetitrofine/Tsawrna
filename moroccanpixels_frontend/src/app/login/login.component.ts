06035c4354af4767ae8ca952ecf4a5981a5a5513
Hamza Ben Yazid <benyazidhamza969@gmail.com>
2022-02-03 16:07:33 +0100

    add jwt cookie setting

--- moroccanpixels_frontend/src/app/login/login.component.ts
+++ moroccanpixels_frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from "../services/authentication.service";
 import {Router} from "@angular/router";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,17 @@ import {Router} from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService:AuthenticationService, private router:Router) { }
+  constructor(private authService:AuthenticationService, private router:Router,private cookieService: CookieService ) { }
 
   ngOnInit(): void {
   }
 
   onLogin(dataForm: any) {
-    this.authService.login(dataForm).subscribe(data =>{
+    this.authService.login(dataForm).subscribe(resp =>{
+      let authToken = resp.headers.get('Authorization')
+      if(authToken!=null){
+        this.cookieService.set( 'Authorization', authToken,15,"/",undefined,true,"Strict");
+      }
       alert("Login successfully")
     },error => alert("there is a problem"))
 
--- moroccanpixels_frontend/src/app/services/authentication.service.ts
+++ moroccanpixels_frontend/src/app/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {User} from "../models/user";
 import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient,HttpResponse} from "@angular/common/http";
 import {UserInfos} from "../models/user-infos";
 
 @Injectable({
@@ -11,9 +11,9 @@ export class AuthenticationService {
   private url="http://localhost:8080"
   constructor(private httpClient: HttpClient) { }
 
-  public login(user:User):Observable<object>{
+  public login(user:User):Observable<HttpResponse<any>>{
     console.log(user)
-    return this.httpClient.post(`${this.url}`+'/login',user);
+    return this.httpClient.post(`${this.url}`+'/login',user,{observe: "response"});
 
   }
   public signup(user:UserInfos):Observable<object>{
