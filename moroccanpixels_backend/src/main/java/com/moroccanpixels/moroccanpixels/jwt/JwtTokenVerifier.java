8fe6a51684d01eddb1f424d404245513a136de21
Hamza Ben Yazid <benyazidhamza969@gmail.com>
2022-02-26 12:30:00 +0100

    changed a property in application.properties

--- moroccanpixels_backend/src/main/java/com/moroccanpixels/moroccanpixels/jwt/JwtTokenVerifier.java
+++ moroccanpixels_backend/src/main/java/com/moroccanpixels/moroccanpixels/jwt/JwtTokenVerifier.java
@@ -21,11 +21,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
+
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class JwtTokenVerifier extends OncePerRequestFilter {
